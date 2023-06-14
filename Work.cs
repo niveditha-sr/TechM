5922713c1cb79ca30babd511fc55cd12116e91c8
niveditha-sr <nivedithaneetha63@gmail.com>
2023-06-14 12:27:36 +0530

    First Commit

---
+++ Work.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProgram
+{
+    internal class Work
+    {
+        public string sayHello()
+        {
+            Console.WriteLine("hi");
+            return "Hello";
+        }
+    }
+}
